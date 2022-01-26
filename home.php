56e64957afa4622a97b64509b55d1e0f40ff8e69
Koykoy200078 <31362410+ChristianCarvs@users.noreply.github.com>
2022-01-26 21:50:53 +0800

    update

--- home.php
+++ home.php
@@ -15,7 +15,7 @@ if(isset($_POST['submit'])) {
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="/design/style.css">
 	<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 	
 <title>Login</title>
